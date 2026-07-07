30e1b7de831f93c61c8a2feec718d79516c0dd36
aprakash97 <prakashakrakr@gmail.com>
2026-07-08 05:21:46 +0530

    revert change

--- src/worker.ts
+++ src/worker.ts
@@ -12,7 +12,7 @@ export default {
   async fetch(request: Request, env: Env) {
     return (
       (await routeAgentRequest(request, env)) ||
-      new Response("Not found", { status: 400 })
+      new Response("Not found", { status: 404 })
     );
   },
 } satisfies ExportedHandler<Env>;
